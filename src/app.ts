7906692fffd151052ebe1425cd289ffae9f39120
Portevent <porte@magous.fr>
2025-12-17 20:12:14 +0100

    fix : remove old app.ts

--- src/app.ts
+++
@@ -1,8 +0,0 @@
-import NodeLeekClient from "./node-leek-client/node-leek-client";
-
-console.log("Starting app");
-
-var nodeLeek = new NodeLeekClient();
-nodeLeek.login("PorteventRemote", "YOUR_PASSWORD").then(() =>
-    nodeLeek.fetchFile(451535, 0).then(file => console.log(file))
-);
